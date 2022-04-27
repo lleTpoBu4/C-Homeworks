a05753ddd72fd856c66ae81b71bf640ab6dc5419
IIeTpoBu4 <petrovichman88@mail.ru>
2022-04-27 15:29:38 +0300

    Task 5 complete

---
+++ Homework 1/Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int n = Int32.Parse(Console.ReadLine());
+int CurrentNumber = -n;
+
+if (n > 0)
+    for (; CurrentNumber <= n;)
+        {
+            Console.Write($"{CurrentNumber}, ");
+            CurrentNumber++;
+        }
+else
+     for (; CurrentNumber >= n;)
+        {
+            Console.Write($"{CurrentNumber}, ");
+            CurrentNumber--;
+        }
