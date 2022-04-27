d8f65b3a23a92c09a78073f86246e8d17c310d78
IIeTpoBu4 <petrovichman88@mail.ru>
2022-04-27 14:48:34 +0300

    Task 4 complete

---
+++ Homework 1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите любое натуральное число: ");
+int n = Int32.Parse(Console.ReadLine());
+int CurrentNumber;
+for (CurrentNumber = 1; CurrentNumber <= n;)
+{
+    if (CurrentNumber % 2 == 0)
+        {
+            Console.Write($"{CurrentNumber}, ");
+            CurrentNumber++;
+        }
+    else
+        CurrentNumber++;
+}
