0d43bbb86d526c1533462f43be3af3d3d82bb47c
IIeTpoBu4 <petrovichman88@mail.ru>
2022-04-27 11:03:21 +0300

    1st task is complete

--- Homework 1/Task1/Program.cs
+++ Homework 1/Task1/Program.cs
@@ -1,3 +1,25 @@
-﻿int a = 1, b = 2
-if a > b
-Concole.Writeline("max = ", a, "min = ", b)
+﻿//Ругается, но работает. Как MVP сойдёт.
+//Думаю, неплохо для первого кода =)
+Console.WriteLine("Введите любое число: ");
+string first = Console.ReadLine();
+Console.WriteLine("Введите второе любое число: ");
+string second = Console.ReadLine();
+double a = Double.Parse(first);
+double b = Double.Parse(second);
+string max = "Максимальное число";
+string min = "Минимальное число";
+if (a > b)
+{
+    Console.WriteLine($"{max} = {a} {min} = {b}");
+}
+else
+{
+    if (a == b)
+    {
+        Console.WriteLine("Введёные числа равны");
+    }
+    else
+    {
+        Console.WriteLine($"{max} = {b} {min} = {a}");
+    }
+}
