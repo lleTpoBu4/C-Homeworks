377942d467e2ba1263e8a9ff644b651d0bf69280
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-02 23:09:32 +0300

    HW2 Task 3 complete

---
+++ Homework 2/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое целое число.");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число.");
+int b = Convert.ToInt32(Console.ReadLine()); 
+int ost = b % a;
+Console.WriteLine(ost);
+if (ost == 0) 
+Console.WriteLine($"Число {b} кратно числу {a}.");
+else
+Console.WriteLine($"Число {b} некратно числу {a}, остаток = {ost}");
