58adedbe55c236305930f383354ae5217802c53c
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-02 19:27:59 +0300

    HW2 Task 1 complete

---
+++ Homework 2/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int GetMax ()
+{
+int number = new Random().Next(10,100);
+int a = number / 10;
+int b = number % 10;
+int max = a;
+if (max < b) max = b;
+return max;
+}
+Console.WriteLine(GetMax());
