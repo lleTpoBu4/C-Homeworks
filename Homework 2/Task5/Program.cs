b9147d44fa5b9baa0b513ec1931cfb1449aaa3eb
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-02 23:48:13 +0300

    HW2 Task 5 complete

---
+++ Homework 2/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое целое число.");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число.");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m)
+Console.WriteLine($"{m} является квадратом {n}");
+else if (m * m == n)
+Console.WriteLine($"{n} является квадратом {m}");
+else
+Console.WriteLine("Ни одно из чисел не является квадратом другого.");
