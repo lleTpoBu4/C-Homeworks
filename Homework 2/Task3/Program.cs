bf967a037e45a5ec64dfb265d8654f8e7b32579f
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-02 23:28:17 +0300

    HW2 Task 4 complete

--- Homework 2/Task3/Program.cs
+++ Homework 2/Task3/Program.cs
@@ -3,7 +3,6 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе целое число.");
 int b = Convert.ToInt32(Console.ReadLine()); 
 int ost = b % a;
-Console.WriteLine(ost);
 if (ost == 0) 
 Console.WriteLine($"Число {b} кратно числу {a}.");
 else
---
+++ Homework 2/Task4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите целое число.");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0 && n % 23 == 0)
+Console.WriteLine($"Число {n} кратно 7 и 23 одновременно.");
+else
+Console.WriteLine($"Число {n} не кратно 7 и 23 одновременно.");
