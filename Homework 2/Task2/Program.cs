49aa4e71e428b4f4700314979fb51b174891b57e
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-02 19:52:57 +0300

    HW2 Task 2 complete

---
+++ Homework 2/Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число из диапазона от 100 до 999.");
+int n = Convert.ToInt32(Console.ReadLine());
+int SecondNum()
+{
+    int second = n / 10 % 10;
+    return second;
+}
+Console.WriteLine($"Второй символ введённого числа - {SecondNum()}");
