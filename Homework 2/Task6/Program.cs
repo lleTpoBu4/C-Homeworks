f6fe5b3a45bdd50e48e030fa4cb0e7a15673420e
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-03 00:14:44 +0300

    HW2 Task 6 complete

---
+++ Homework 2/Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите целое число из диапазона от -1000 до -9999.");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = -n / 10 % 10;
+if (n < -9999 || n > -1000)
+Console.WriteLine("Введённое число не соответствует условию задачи.");  
+else 
+Console.WriteLine($"{result} - вторая цифра этого числа.");
