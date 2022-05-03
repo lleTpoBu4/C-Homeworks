af8c00d717970377b78748e6038e1bc642b35ef8
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-03 20:03:22 +0300

    HW2 Task7 complete

---
+++ Homework 2/Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ввод числа с преобразованием в int
+//Проверка на положительность и инверсия при необходимости.
+//нахождение количества символов
+//если символов меньше 3, то сообщение: "третьей цифры нет"
+//иначе - вывод третьей цифры.
+Console.WriteLine("Введите целое число.");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0) n = -n;
+string numbers = Convert.ToString(n);
+int[] array = new int[numbers.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = numbers[i] - '0';
+    Console.WriteLine(array[i]);
+}
+if (array.Length < 3)
+    Console.WriteLine("Третьей цифры нет.");
+else Console.WriteLine($"Третья цифра заданного числа - {array[2]}");
