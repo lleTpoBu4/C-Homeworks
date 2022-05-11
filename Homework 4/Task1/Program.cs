120bd7fd710b1df0535dcbecf9f7395d077cde40
IIeTpoBu4 <petrovichman88@mail.ru>
2022-05-11 12:20:21 +0300

    HW4 (Minimum requirements) complete

---
+++ Homework 4/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите любое целое число:");
+double A = Convert.ToDouble(Console.ReadLine()); //Ввод числа
+A = Math.Round(A); // округление до целого (а то мало ли...)
+//Написание метода (Найти сумму арифметической прогрессии от 1 до n):
+//Для n > 0
+double SumArProgPos(double n)
+{
+    double result = (1 + n) / 2 * n;
+    return result;
+}
+
+//Для n < 0
+double SumArProgCons(double n)
+{   double result = (1+n)/2*(-n+2);
+    return result;
+}
+
+
+//Вывод результата вычислений:
+if (A > 0)
+Console.WriteLine($"Cумма чисел от 1 до {A} равна: {SumArProgPos(A)}");
+else
+Console.WriteLine($"Cумма чисел от 1 до {A} равна: {SumArProgCons(A)}");
